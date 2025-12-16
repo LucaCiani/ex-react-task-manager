8a58e25443ff968b9be6d5656dfb52c4a3da16f7
LucaCiani <lucaciani88@gmail.com>
2025-12-16 15:45:14 +0100

    add globalcontext

--- src/App.jsx
+++ src/App.jsx
@@ -6,55 +6,61 @@ import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import HomePage from "./assets/pages/HomePage";
 import TaskListPage from "./assets/pages/TaskListPage";
 import AddListPage from "./assets/pages/AddListPage";
+import { GlobalProvider } from "./assets/GlobalContext";
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <NavLink className="navbar-brand" to="/">
-                            Homepage
-                        </NavLink>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div
-                            className="collapse navbar-collapse"
-                            id="navbarSupportedContent"
-                        >
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <NavLink
-                                        className="nav-link"
-                                        to="task-list"
-                                    >
-                                        Tasks
-                                    </NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="add-task">
-                                        Add Task
-                                    </NavLink>
-                                </li>
-                            </ul>
+            <GlobalProvider>
+                <BrowserRouter>
+                    <nav className="navbar navbar-expand-lg bg-body-secondary">
+                        <div className="container-fluid">
+                            <NavLink className="navbar-brand" to="/">
+                                Homepage
+                            </NavLink>
+                            <button
+                                className="navbar-toggler"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div
+                                className="collapse navbar-collapse"
+                                id="navbarSupportedContent"
+                            >
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <NavLink
+                                            className="nav-link"
+                                            to="task-list"
+                                        >
+                                            Tasks
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink
+                                            className="nav-link"
+                                            to="add-task"
+                                        >
+                                            Add Task
+                                        </NavLink>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </nav>
-                <Routes>
-                    <Route path="/" Component={HomePage} />
-                    <Route path="task-list" Component={TaskListPage} />
-                    <Route path="add-task" Component={AddListPage} />
-                </Routes>
-            </BrowserRouter>
+                    </nav>
+                    <Routes>
+                        <Route path="/" Component={HomePage} />
+                        <Route path="task-list" Component={TaskListPage} />
+                        <Route path="add-task" Component={AddListPage} />
+                    </Routes>
+                </BrowserRouter>
+            </GlobalProvider>
         </>
     );
 }
---
+++ src/assets/GlobalContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useEffect, useState } from "react";
+
+export const GlobalContext = createContext();
+
+const { VITE_API_URL } = import.meta.env;
+
+export function GlobalProvider({ children }) {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch(`${VITE_API_URL}/tasks`)
+            .then((res) => res.json)
+            .then((data) => setTasks(data))
+            .catch((err) => console.error(err));
+    }, []);
+
+    return (
+        <GlobalContext.Provider value={{ tasks, setTasks }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+}
