40f5a6036dbcca00771a00354582b5a90eed814a
LucaCiani <lucaciani88@gmail.com>
2025-12-29 16:58:11 +0100

    Milestone 7

--- src/assets/pages/TaskDetailPage.jsx
+++ src/assets/pages/TaskDetailPage.jsx
@@ -1,1 +1,36 @@
-export default function TaskDetailPage() {}
+import { useContext } from "react";
+import { GlobalContext } from "../../GlobalContext";
+import { useParams } from "react-router-dom";
+
+export default function TaskDetailPage() {
+    const { tasks } = useContext(GlobalContext);
+    const { id } = useParams();
+
+    const singleTask = tasks.find((task) => task.id == id);
+
+    return (
+        <>
+            <div className="container">
+                <div className="card my-5 border border-0">
+                    <div className="card-body">
+                        <h3 className="card-title">{singleTask.title}</h3>
+                        {singleTask.description && (
+                            <p className="card-text">
+                                <b>Descrizione:</b> {singleTask.description}
+                            </p>
+                        )}
+                        <p className="card-text">
+                            <b>Stato:</b> {singleTask.description}
+                        </p>
+                        <p className="card-text">
+                            <b>Data di creazione:</b>{" "}
+                            {new Date(
+                                singleTask.createdAt
+                            ).toLocaleDateString()}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
